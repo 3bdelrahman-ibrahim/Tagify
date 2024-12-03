d6fe704e3206a0068a79e89c2ee388768ad44f33
3bdelrahman.ibrahim <3bdelrahman.ibrahim@gmail.com>
2024-12-03 16:39:54 +0200

    Added local files

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(express.json());
 
 // MongoDB connection using environmssent variable
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.log('Error connecting to MongoDB:', err));
